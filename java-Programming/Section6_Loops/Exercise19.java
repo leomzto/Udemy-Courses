976a00eb2ac364fbca3ab05ee4c231ee0b52f3c2
LeoManzato <leopmanzato@gmail.com>
2025-12-15 21:08:45 -0300

    Section 6 completed

---
+++ java-Programming/Section6_Loops/Exercise19.java
@@ -0,0 +1,17 @@
+package Section6_Loops;
+
+public class Exercise19 {
+
+    public static void main (String[] args) {
+
+        int i = 5;
+
+        while ( !(i < 1) ) {
+            System.out.println( i );
+            i--;
+        }
+
+    }
+
+}
+
---
+++ java-Programming/Section6_Loops/Exercise20.java
@@ -0,0 +1,14 @@
+package Section6_Loops;
+
+public class Exercise20 {
+
+    public static void main (String[] args) {
+
+        for(int i = 5; i > 0; i--) {
+            System.out.println( i );
+        }
+
+    }
+
+}
+
